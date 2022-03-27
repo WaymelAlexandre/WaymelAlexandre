06d55d2928f7526429758fb1d2b6ecac496f72cd
AleWaymelAlexandre <axl.witw@gmail.com>
2022-03-27 17:34:07 +1100

    js file

---
+++ java.js
@@ -0,0 +1,12 @@
+
+// burger menue funger 
+function myFunction(x) {
+    x.classList.toggle("change");
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle('active');
+  }
+  
+  function DARK() {
+     var element = document.body;
+     element.classList.toggle("dark-mode");
+  } 
