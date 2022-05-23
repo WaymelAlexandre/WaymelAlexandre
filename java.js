ff45b51d88938c03594e9102d5fa2b10d416940a
AleWaymelAlexandre <axl.witw@gmail.com>
2022-05-23 16:13:09 +1000

    still need some work but a can see the light

--- java.js
+++ java.js
@@ -1,12 +1,49 @@
+const container = document.querySelector(".container");
+const sidebar = document.querySelector(".sidebar");
+const menu = document.querySelector(".menu");
+let MOpen = false;
+
 
 // burger menue funger 
-function myFunction(x) {
-    x.classList.toggle("change");
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.classList.toggle('active');
-  }
+
+
+container.addEventListener("click", () => {
+   MOpen = !MOpen;
+
+   if(MOpen){
+      container.classList.add("open");
+      sidebar.classList.toggle("menu-open");
+      menu.classList.toggle("menu-open");
+   }
+   else{
+      container.classList.remove("open");
+      sidebar.classList.remove("menu-open");
+      menu.classList.remove("menu-open");
+   }
   
-  function DARK() {
-     var element = document.body;
-     element.classList.toggle("dark-mode");
-  } 
+})
+
+ 
+function myFunction(x) {
+   x.classList.toggle("change");
+   sidebar.classList.toggle('active');
+ }
+
+
+
+
+
+// function myFunction(x) {
+//     x.classList.toggle("change");
+    
+//     sidebar.classList.toggle('active');
+//   }
+
+  
+
+
+
+//   function DARK() {
+//      var element = document.body;
+//      element.classList.toggle("dark-mode");
+//   } 
